b984d10f0be96a211dd3c7ddabf12eac1df51b4b
Andrea Casero <41995796+WINTERANDREA@users.noreply.github.com>
2019-12-09 10:51:06 +0100

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -5,12 +5,12 @@ from email.mime.text import MIMEText
 def send_mail(customer, dealer, rating, comments):
     port = 2525
     smtp_server = 'smtp.mailtrap.io'
-    login = '1fe3cc4024cb1e'
-    password = '09851704cd2ed8'
+    login = 'Insert your username'
+    password = 'Insert your password'
     message = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
 
-    sender_email = 'andrecasero@gmail.com'
-    receiver_email = 'andrea91winter@hotmail.it'
+    sender_email = 'Insert your email 1'
+    receiver_email = 'Insert your email 2'
     msg = MIMEText(message, 'html')
     msg['Subject'] = 'FTB Feedback'
     msg['From'] = sender_email
